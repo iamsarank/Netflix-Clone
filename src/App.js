fb3f7ce380018c04d187b593095d34bc95700e8f
Madurai Corporation <itzsarank@gmail.com>
2022-12-06 19:29:39 +0530

    firebase setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Netflix from "./pages/Netflix";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+        <Routes>
+            <Route exact path="/login" element={<Login />}/>
+            <Route exact path="/signup" element={<Signup />}/>
+            <Route exact path="/" element={<Netflix />}/>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
